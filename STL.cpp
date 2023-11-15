8b73ed4d90c68651f6699b837764e2708ac2b120
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-15 19:23:15 +0100

    .

--- STL.cpp
+++ STL.cpp
@@ -2,3 +2,15 @@
 #include <vector>
 #include <map>
 #include <set>
+
+int main() {
+    // Vector
+    std::vector<int> numeros {1, 2, 3, 4, 5};
+
+    numeros.push_back(6);
+
+    std::cout << "Vector: ";
+    for (const auto& num : numeros) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
