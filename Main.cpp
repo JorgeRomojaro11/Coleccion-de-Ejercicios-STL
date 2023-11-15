e9a40c0fd5f328ce1466ba92a05cc6c5d134717b
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-15 19:17:33 +0100

    .

--- Main.cpp
+++ Main.cpp
@@ -3,3 +3,12 @@
 #include "Círculo.cpp"
 #include "Rombo.cpp"
 #include "Trapecio.cpp"
+
+int main() {
+    std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(8, 3) << std::endl;
+    std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(7) << std::endl;
+    std::cout << "Area del rombo: " << Geometria::calcularAreaRombo(4, 6) << std::endl;
+    std::cout << "Area del trapecio: " << Geometria::calcularAreaTrapecio(9, 2, 5) << std::endl;
+
+    return 0;
+}
